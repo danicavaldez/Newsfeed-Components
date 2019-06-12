b82392f21db670b1c671d42ec6280fabb21c21cb
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-06-11 19:30:09 -0700

    added collapse button

--- Article/Article.js
+++ Article/Article.js
@@ -6,14 +6,21 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector(".expandButton");
+    this.collapseButton = domElement.querySelector(".collapseButton");
+    this.buttonsGroup = domElement.querySelector(".article-buttons")
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
+    this.collapseButton.textContent = "collapse";
+    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
+    this.buttonsGroup.addEventListener("click", this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+   
+    this.expandButton.classList.toggle("hide-btn");
+    this.collapseButton.classList.toggle("hide-btn");
     this.domElement.classList.toggle("article-open");
   }
 }
